d1ae8650f924e818508667ba7965d499531fa2e9
Mas-Pey <konamexxxpes@gmail.com>
2025-12-10 20:55:50 +0700

    chore(di): register command handler and plane command

--- internal/bootstrap/application.go
+++ internal/bootstrap/application.go
@@ -3,9 +3,15 @@ package bootstrap
 import (
 	"plane-discord-bot/internal/application"
 	"plane-discord-bot/internal/application/api"
+	"plane-discord-bot/internal/application/commands"
 )
 
 func RegisterApi() {
 	appContainer.RegisterService("planeHandler", &api.PlaneHandler{})
 	appContainer.RegisterService("api", &application.Api{})
+}
+
+func RegisterApplication() {
+	appContainer.RegisterService("commandHandler", &commands.CommandHandler{})
+	appContainer.RegisterService("planeCommand", &commands.PlaneCommand{})
 }
